4b872e9b1288782cb72896d6f51c7a8f00e2e679
a3sss <a3sss@MSI>
2025-12-13 19:06:16 +0100

    Agregar archivos de proyecto.

---
+++ scr/main.cpp
@@ -0,0 +1,561 @@
+#include "BOX.h"
+#include "auxiliar.h"
+
+#include <gl/glew.h>
+#define SOLVE_FGLUT_WARNING
+#include <gl/freeglut.h> 
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+
+
+// === Datos que se almacenan en la memoria de la CPU ===
+
+//Matrices
+glm::mat4 proj = glm::mat4(1.0f);
+glm::mat4 view = glm::mat4(1.0f);
+glm::mat4 model = glm::mat4(1.0f);
+
+//Variables de camara
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 6.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+float fov = 60.0f;
+
+
+
+// === Variables que nos dan acceso a Objetos OpenGL ===
+
+
+//Shaders
+unsigned int vshader;
+unsigned int fshader;
+unsigned int program;
+
+//Variables Uniform
+int uModelViewMat;
+int uModelViewProjMat;
+int uNormalMat;
+
+//Atributos
+int inPos;
+int inColor;
+int inNormal;
+int inTexCoord;
+
+//Texturas
+unsigned int colorTexId;
+unsigned int emiTexId;
+
+//Texturas Uniform
+int uColorTex;
+int uEmiTex;
+
+//VAO
+unsigned int vao;
+
+//VBOs que forman parte del objeto
+unsigned int posVBO;
+unsigned int colorVBO;
+unsigned int normalVBO;
+unsigned int texCoordVBO;
+unsigned int triangleIndexVBO;
+
+//UBOs
+unsigned int uboLight; //ID del UBO
+const unsigned int LIGHT_BINDING_INDEX = 0; //binding point
+
+//Control de ratón
+bool rightMouseDown = false;
+int lastMouseX = 0;
+int lastMouseY = 0;
+
+//Structs
+//Light data
+struct lightData {
+	glm::vec3 Ia; float pad0;
+	glm::vec3 Id; float pad1;
+	glm::vec3 Is; float pad2;
+	glm::vec3 lpos;
+};
+
+// === Funciones auxiliares ===
+//!!Por implementar
+
+//Declaración de CB
+void renderFunc();
+void resizeFunc(int width, int height);
+void timerFunc(int value); //Sustituye a IdleFunc para fijar fps
+void keyboardFunc(unsigned char key, int x, int y);
+void mouseFunc(int button, int state, int x, int y);
+void motionFunc(int x, int y);
+void mouseWheelFunc(int button, int direction, int x, int y);
+
+// === Funciones de inicialización y destrucción ===
+void initContext(int argc, char** argv);
+void initOGL();
+void initShader(const char *vname, const char *fname);
+void initObj();
+void destroy();
+void initUBOs();
+
+
+// === Carga el shader indicado, devuele el ID del shader ===
+//!Por implementar
+GLuint loadShader(const char *fileName, GLenum type);
+
+//Crea una textura, la configura, la sube a OpenGL, 
+//y devuelve el identificador de la textura 
+//!!Por implementar
+unsigned int loadTex(const char *fileName);
+
+
+
+
+
+
+int main(int argc, char** argv)
+{
+	std::locale::global(std::locale("spanish"));// acentos ;)
+
+	initContext(argc, argv);
+	initOGL();
+	initShader("../shaders_P3/shader.v1.vert", "../shaders_P3/shader.v1.frag");
+
+	//UBOs
+	initUBOs();
+
+	initObj();
+
+	glutMainLoop();
+
+	destroy();
+
+	return 0;
+}
+	
+  
+ // === Funciones auxiliares ===
+void initContext(int argc, char** argv){
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 3);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	//glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Prácticas GLSL");
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		std::cout << "Error: " << glewGetErrorString(err) << std::endl;
+		exit(-1);
+	}
+	const GLubyte* oglVersion = glGetString(GL_VERSION);
+	std::cout << "This system supports OpenGL Version: " << oglVersion << std::endl;
+
+	//Callbacks
+	glutReshapeFunc(resizeFunc);
+	glutDisplayFunc(renderFunc);
+	//glutIdleFunc(idleFunc);
+	glutTimerFunc(0, timerFunc, 0); //Sustituye a IdleFunc para fijar fps
+	glutKeyboardFunc(keyboardFunc);
+	glutMouseFunc(mouseFunc);
+	glutMotionFunc(motionFunc);
+	glutMouseWheelFunc(mouseWheelFunc);
+
+}
+void initOGL(){
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+
+	glFrontFace(GL_CCW);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glEnable(GL_CULL_FACE);
+
+	proj = glm::perspective(glm::radians(60.0f), 1.0f, 0.1f, 50.0f);
+	view = glm::mat4(1.0f);
+	view[3].z = -6; //inversa de la posición de la cámara (se mueve el mundo)
+
+
+
+}
+void destroy(){
+	glDetachShader(program, vshader);
+	glDetachShader(program, fshader);
+	glDeleteShader(vshader);
+	glDeleteShader(fshader);
+	glDeleteProgram(program);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	if (inPos != -1) glDeleteBuffers(1, &posVBO);
+	if (inColor != -1) glDeleteBuffers(1, &colorVBO);
+	if (inNormal != -1) glDeleteBuffers(1, &normalVBO);
+	if (inTexCoord != -1) glDeleteBuffers(1, &texCoordVBO);
+	glDeleteBuffers(1, &triangleIndexVBO);
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &vao);
+
+	glDeleteTextures(1, &colorTexId);
+	glDeleteTextures(1, &emiTexId);
+}
+void initShader(const char *vname, const char *fname){
+	vshader = loadShader(vname, GL_VERTEX_SHADER);
+	fshader = loadShader(fname, GL_FRAGMENT_SHADER);
+
+	program = glCreateProgram();
+	glAttachShader(program, vshader);
+	glAttachShader(program, fshader);
+
+	//Identificadores atributos
+	glBindAttribLocation(program, 0, "inPos");
+	glBindAttribLocation(program, 1, "inColor");
+	glBindAttribLocation(program, 2, "inNormal");
+	glBindAttribLocation(program, 3, "inTexCoord");
+
+	glLinkProgram(program);
+
+	int linked;
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if (!linked)
+	{
+		//Calculamos una cadena de error
+		GLint logLen;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLen);
+		char* logString = new char[logLen];
+		glGetProgramInfoLog(program, logLen, NULL, logString);
+		std::cout << "Error: " << logString << std::endl;
+		delete logString;
+		glDeleteProgram(program);
+		program = 0;
+		exit(-1);
+	}
+
+	//Identificadores variables uniformes
+	uNormalMat = glGetUniformLocation(program, "normal");
+	uModelViewMat = glGetUniformLocation(program, "modelView");
+	uModelViewProjMat = glGetUniformLocation(program, "modelViewProj");
+
+	//Texturas
+	uColorTex = glGetUniformLocation(program, "colorTex");
+	uEmiTex = glGetUniformLocation(program, "emiTex");
+
+	//CREAR identificadores de atributos 
+	inPos = glGetAttribLocation(program, "inPos");
+	inColor = glGetAttribLocation(program, "inColor");
+	inNormal = glGetAttribLocation(program, "inNormal");
+	inTexCoord = glGetAttribLocation(program, "inTexCoord");
+
+	//Enlace light UBO
+	GLuint lightBlockIndex = glGetUniformBlockIndex(program, "lightBlock"); //Obtenemos el índice del bloque
+	if (lightBlockIndex == GL_INVALID_INDEX) {
+		std::cerr << "No se ha encontrado el bloque de luces" << std::endl;
+	}
+	else {
+		glUniformBlockBinding(program, lightBlockIndex, LIGHT_BINDING_INDEX);
+	}
+
+}
+void initObj(){
+	//VAO
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//Posiciones
+	if (inPos != -1)
+	{
+		glGenBuffers(1, &posVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, posVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 3,
+			cubeVertexPos, GL_STATIC_DRAW);
+		glVertexAttribPointer(inPos, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inPos);
+	}
+	//Colores
+	if (inColor != -1)
+	{
+		glGenBuffers(1, &colorVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 3,
+			cubeVertexColor, GL_STATIC_DRAW);
+		glVertexAttribPointer(inColor, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inColor);
+	}
+	//Normales
+	if (inNormal != -1)
+	{
+		glGenBuffers(1, &normalVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, normalVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 3,
+			cubeVertexNormal, GL_STATIC_DRAW);
+		glVertexAttribPointer(inNormal, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inNormal);
+	}
+	//Texturas
+	if (inTexCoord != -1)
+	{
+		glGenBuffers(1, &texCoordVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, texCoordVBO);
+		glBufferData(GL_ARRAY_BUFFER, cubeNVertex * sizeof(float) * 2,
+			cubeVertexTexCoord, GL_STATIC_DRAW);
+		glVertexAttribPointer(inTexCoord, 2, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(inTexCoord);
+	}
+
+	//Indices de los triángulos (Index Buffer)
+	glGenBuffers(1, &triangleIndexVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangleIndexVBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		cubeNTriangleIndex * sizeof(unsigned int) * 3, cubeTriangleIndex,
+		GL_STATIC_DRAW);
+
+	//Model matrix
+	model = glm::mat4(1.0f);
+
+	//Dirección de las texturas
+	colorTexId = loadTex("../img/color2.png");
+	emiTexId = loadTex("../img/emissive.png");
+}
+
+GLuint loadShader(const char *fileName, GLenum type){ 
+	unsigned int fileLen;
+	char* source = loadStringFromFile(fileName, fileLen);
+	//Creación y compilación del Shader
+	GLuint shader;
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1,
+		(const GLchar**)&source, (const GLint*)&fileLen);
+	glCompileShader(shader);
+	delete source;
+	
+	//Comprobamos que se compiló bien
+	GLint compiled;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+	if (!compiled)
+	{
+		//Calculamos una cadena de error
+		GLint logLen;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLen);
+
+		char* logString = new char[logLen];
+		glGetShaderInfoLog(shader, logLen, NULL, logString);
+		std::cout << "Error: " << logString << std::endl;
+		delete logString;
+
+		glDeleteShader(shader);
+		exit(-1);
+	}
+
+	return shader;
+}
+unsigned int loadTex(const char *fileName){
+
+	unsigned char* map;
+	unsigned int w, h;
+	map = loadTexture(fileName, w, h);
+	if (!map)
+	{
+		std::cout << "Error cargando el fichero: "
+			<< fileName << std::endl;
+		exit(-1);
+	}
+
+	unsigned int texId;
+	glGenTextures(1, &texId);
+	glBindTexture(GL_TEXTURE_2D, texId);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_RGBA,
+		GL_UNSIGNED_BYTE, (GLvoid*)map);
+
+	delete[] map;
+
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+	GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+
+	return texId;
+}
+
+void renderFunc(){
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(program);
+
+	view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+	
+	glm::mat4 modelView = view * model;
+	glm::mat4 modelViewProj = proj * view * model;
+	glm::mat4 normal = glm::transpose(glm::inverse(modelView));
+	if (uModelViewMat != -1)
+		glUniformMatrix4fv(uModelViewMat, 1, GL_FALSE,
+			&(modelView[0][0]));
+	if (uModelViewProjMat != -1)
+		glUniformMatrix4fv(uModelViewProjMat, 1, GL_FALSE,
+			&(modelViewProj[0][0]));
+	if (uNormalMat != -1)
+		glUniformMatrix4fv(uNormalMat, 1, GL_FALSE,
+			&(normal[0][0]));
+
+	//Texturas
+	if (uColorTex != -1)
+	{
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, colorTexId);
+		glUniform1i(uColorTex, 0);
+	}
+	if (uEmiTex != -1)
+	{
+		glActiveTexture(GL_TEXTURE0 + 1);
+		glBindTexture(GL_TEXTURE_2D, emiTexId);
+		glUniform1i(uEmiTex, 1);
+	}
+
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, cubeNTriangleIndex * 3, GL_UNSIGNED_INT, (void*)0);
+
+
+	glUseProgram(NULL);
+
+	glutSwapBuffers();
+
+}
+void resizeFunc(int width, int height) {
+	glViewport(0, 0, width, height);
+	glutPostRedisplay();
+}
+void timerFunc(int value) {
+	// Lógica de actualización (animación del cubo)
+	model = glm::mat4(1.0f);
+	static float angle = 0.0f;
+	angle = (angle > 3.141592f * 2.0f) ? 0 : angle + 0.01f;
+	model = glm::rotate(model, angle, glm::vec3(1.0f, 1.0f, 0.0f));
+
+	glutPostRedisplay();
+
+	// Volver a llamar a esta función en 16ms (aprox 60 FPS)
+	glutTimerFunc(1000 / 60, timerFunc, 0);
+}
+
+void keyboardFunc(unsigned char key, int x, int y){
+	float cameraSpeed = 0.1f;
+	//Movimiento hacia delante
+	if (key == 'w' || key == 'W')
+	{
+		cameraPos += cameraSpeed * cameraFront;
+	}
+	//Movimiento hacia atrás
+	else if (key == 's' || key == 'S')
+	{
+		cameraPos -= cameraSpeed * cameraFront;
+	}
+	//Movimiento izquierda
+	else if (key == 'a' || key == 'A')
+	{
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+	//Movimiento derecha
+	else if (key == 'd' || key == 'D')
+	{
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+}
+void mouseFunc(int button, int state, int x, int y){
+	if (button == GLUT_RIGHT_BUTTON)
+	{
+		if (state == GLUT_DOWN) {
+			rightMouseDown = true;
+			lastMouseX = x;
+			lastMouseY = y;
+		}
+		else {
+			rightMouseDown = false;
+		}
+
+	}
+
+}
+
+void motionFunc(int x, int y) {
+	if (rightMouseDown) {
+		float xoffset = x - lastMouseX;
+		float yoffset = lastMouseY - y; 
+		lastMouseX = x;
+		lastMouseY = y;
+
+		float sensitivity = 0.2f;
+		xoffset *= sensitivity;
+		yoffset *= sensitivity;
+
+		yaw += xoffset;
+		pitch += yoffset;
+
+		// Restricciones para no dar la vuelta completa
+		if (pitch > 89.0f) pitch = 89.0f;
+		if (pitch < -89.0f) pitch = -89.0f;
+
+		// Calcular el nuevo vector frontal
+		glm::vec3 front;
+		front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		front.y = sin(glm::radians(pitch));
+		front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		cameraFront = glm::normalize(front);
+
+		glutPostRedisplay();
+	}
+}
+void mouseWheelFunc(int wheel, int direction, int x, int y) {
+	// direction: +1 (hacia adelante/arriba), -1 (hacia atrás/abajo)
+	if (direction > 0)
+		fov -= 2.0f; // Zoom in (reducir FOV)
+	else
+		fov += 2.0f; // Zoom out (aumentar FOV)
+
+	// Limitar el zoom para evitar que se invierta o sea demasiado grande
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 90.0f)
+		fov = 90.0f;
+
+	// Recalcular la matriz de proyección con el nuevo FOV
+	proj = glm::perspective(glm::radians(fov), 1.0f, 0.1f, 50.0f);
+
+	glutPostRedisplay();
+}
+void initUBOs() {
+	//Crear datos de la luz
+	lightData lightData;
+	lightData.Ia = glm::vec3(0.3f);
+	lightData.Id = glm::vec3(1.0f);
+	lightData.Is = glm::vec3(1.0f);
+	lightData.lpos = glm::vec3(0.0f);
+
+	//=== CREAR EL UBO ===
+
+	//Generar el buffer
+	glGenBuffers(1, &uboLight);
+	glBindBuffer(GL_UNIFORM_BUFFER, uboLight);
+
+	//Asignar espacio y copiar los datos
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(lightData), &lightData, GL_STATIC_DRAW);
+
+	//Enlazar el buffer al binding point
+	glBindBufferBase(GL_UNIFORM_BUFFER, LIGHT_BINDING_INDEX, uboLight);
+
+
+	//Desenlazar el buffer
+	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+}
